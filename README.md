163e37a4a28dfeeb324d60e1fe68667b906bd944
Tracey Jaquith <tracey_pooh@yahoo.com>
2018-06-17 01:50:02 -0700

    installation details

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # chordify-chrome-extension
 cosmetically adjusts JS/CSS chordify.net pages to have large video and be more like riffstation
+
+# installation:
+- open `terminal.app` on mac or similar
+- `git clone https://github.com/traceypooh/chordify-chrome-extension.git`
+- in `chrome` browser, navigate to:
+  - `chrome://extensions/`
+  - toggle on `Developer mode` in upper right (if not already on)
+  - click the `LOAD UNPACKED` link in top center
+  - find this folder `chordify-chrome-extension`
+  - hit `Select`
+- If you notice any timing issues, you can trigger a rerun of the JS/CSS via a browser tab resize
+
---
+++ chordify.js
@@ -0,0 +1,48 @@
+// so good!  https://developer.chrome.com/extensions/content_scripts
+
+$(window).on('load  resize  orientationchange', () => {
+  clearTimeout(window.chordify_throttler)
+  window.chordify_throttler = setTimeout(() => {
+    const topht = 555
+
+    // hide top ads from view
+    window.enableAd = false
+    $('#mm, #adContent, #google_center_div').css({
+      height: 0,
+      overflowY: 'hidden',
+      'max-height': 0
+    })
+
+    // declutter and get side video higher up
+    $('#suggestions, #footer, .ios-banner').remove()
+
+    // get main content to top
+    $('#content').css({'margin-top': 50})
+
+    // shrink height of chords area
+    $('.diagrams-guitar').css({'margin-bottom': 0, height: 300})
+
+    // center main column
+    $('#song').css('float', 'none')
+
+    // move video to fill entire bottom of page and make bigga!
+    const e = $('#youtube')
+    e.css({
+      position: 'fixed',
+      top: topht + 'px',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      width: '100%',
+      height: '' + (window.innerHeight - topht) + 'px'
+    })
+    // kick on controls, in case video has CC lyrics and may want to show it, etc.
+    const con = () => (e  &&  e.attr('src')  &&  e.attr('src').match(/controls=0/) ?
+      e.attr('src', e.attr('src').replace(/&controls=0/, '&controls=1')) : 'no-vid')
+    con()
+    setTimeout(con, 3000)
+
+    console.log('--- [pooh chordify extension alterations done] ---')
+  },
+  1500) // run 1.5s after event (and dont run more frequently than that (eg: many resize events))
+})
